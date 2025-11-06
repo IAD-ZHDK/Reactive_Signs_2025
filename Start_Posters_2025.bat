90c471fb3efb4b2a60d234e70e708d14ffb720d8
Luke Franzke <lukefranzke@gmail.com>
2025-11-06 11:36:39 +0100

    small update

--- Start_Posters_2025.bat
+++ Start_Posters_2025.bat
@@ -12,7 +12,6 @@ cd "C:\Users\User\Desktop\Reactive_Signs_2025\cameraPoseOSC\" &
 start cmd /k ".\venv\Scripts\activate.bat && python -u pose_detector_yoloV8.py --gpu 0"
 echo Python pose detector started in new window 
 
-
 TIMEOUT /t 10
 echo npm version:
 CMD /C npm --version
@@ -23,8 +22,31 @@ echo ensure that the port is free
 CMD /C npx kill-port --port 8081 
 
 TIMEOUT /t 2
-echo starting Chrome in fullscreen mode
-start chrome --start-fullscreen --disable-session-crashed-bubble --disable-infobars --disable-restore-session-state http://localhost:8081
+echo starting Chrome in kiosk mode
+start chrome --kiosk http://localhost:8081 ^
+  --no-first-run ^
+  --no-default-browser-check ^
+  --no-pings ^
+  --no-service-autorun ^
+  --disable-background-networking ^
+  --disable-breakpad ^
+  --disable-client-side-phishing-detection ^
+  --disable-component-extensions-with-background-pages ^
+  --disable-component-update ^
+  --disable-default-apps ^
+  --disable-device-discovery-notifications ^
+  --disable-extensions ^
+  --disable-features=InterestFeedContentSuggestions ^
+  --disable-geolocation ^
+  --disable-preconnect ^
+  --disable-sync ^
+  --enable-automation ^
+  --no-crash-upload ^
+  --disable-session-crashed-bubble ^
+  --disable-infobars ^
+  --disable-restore-session-state ^
+  --no-default-browser-check ^
+  --disk-cache-dir=nul
 
 echo changing to exhibition directory
 cd "C:\Users\User\Desktop\Reactive_Signs_2025\2025_Exhibition\"
